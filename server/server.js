271025514288e0f22ed4a017b11e5634cdfe868e
c4w <chen4w@sina.com>
2018-09-17 01:15:32 +0800

    upload by post

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const path = require('path');
+const { Prisma } = require('prisma-binding')
 const multer = require('multer');
 const upload = multer({
   dest: 'uploads/' // this saves your file into a directory called "uploads"
 }); 
 
+const pdb = new Prisma({
+    typeDefs: '../gdb/src/generated/prisma.graphql', // the auto-generated GraphQL schema of the Prisma API
+    endpoint: 'http://localhost:4466', // the endpoint of the Prisma API
+    debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+    // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
+  });
+  
 const app = express();
 const port = process.env.PORT || 5000;
 // API calls
@@ -19,6 +26,11 @@ app.post('/upload', upload.single('file-to-upload'), (req, res) => {
     console.log('文件大小：%s', file.size);
     console.log('文件保存路径：%s', file.path);
     res.send({ret_code: '0'});
+    pdb.mutation.createFile(
+        {
+          data: {title:file.originalname}
+        }
+      )    
   });
   
 app.listen(port, () => console.log(`Listening on port ${port}`));
