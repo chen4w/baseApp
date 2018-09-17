ee775a0fc23cea1ad406337d6f12785edfdb7431
chen4w <chen4w@c4w-mbp.local>
2018-09-17 14:01:33 +0800

    file upload & download

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Prisma } = require('prisma-binding')
 const multer = require('multer');
 const upload = multer({
-  dest: 'uploads/' // this saves your file into a directory called "uploads"
+  dest: '../public/uploads/' // this saves your file into a directory called "uploads"
 }); 
 
 const pdb = new Prisma({
@@ -18,17 +18,14 @@ const port = process.env.PORT || 5000;
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
-
+app.use('/files', express.static('uploads'));
 app.post('/upload', upload.single('file-to-upload'), (req, res) => {
     var file = req.file;
-    console.log('文件类型：%s', file.mimetype);
-    console.log('原始文件名：%s', file.originalname);
-    console.log('文件大小：%s', file.size);
-    console.log('文件保存路径：%s', file.path);
     res.send({ret_code: '0'});
     pdb.mutation.createFile(
         {
-          data: {title:file.originalname}
+          data: {title:file.originalname,size:file.size,
+            contentType:file.mimetype,url:'/uploads/'+file.filename}
         }
       )    
   });
--- src/components/file/index.js
+++ src/components/file/index.js
@@ -1,65 +1,81 @@
-import React from 'react';
+import React from "react";
 import {
-    ShowButton, Show,
-    SimpleShowLayout,BooleanField,ImageInput,FileField,
-    ImageField,
-    Filter, DateField, DateInput, Responsive, SimpleList, List, Edit, Create, Datagrid, TextField, EditButton, DisabledInput, SimpleForm, TextInput
-} from 'react-admin/lib';
+  ShowButton,
+  Show,
+  SimpleShowLayout,
+  UrlField,
+  FileInput,
+  FileField,
+  Filter,
+  DateField,
+  DateInput,
+  Responsive,
+  SimpleList,
+  List,
+  Edit,
+  Create,
+  Datagrid,
+  TextField,
+  EditButton,
+  DisabledInput,
+  SimpleForm,
+  TextInput
+} from "react-admin/lib";
 
 const FileFilter = props => (
-    <Filter {...props}>
-        <TextInput label="name" source="name" alwaysOn />
-    </Filter>
+  <Filter {...props}>
+    <TextInput label="name" source="name" alwaysOn />
+  </Filter>
 );
 
-export const FileList = (props) => (
-    <List {...props}
-        filters={<FileFilter />}
-        bulkActions={false}
-        sort={{ field: 'id', order: 'ASC' }}>
-        <Responsive
-            small={
-                <SimpleList
-                    primaryText={record => record.id}
-                    secondaryText={record => record.name}
-                />
-            }
-            medium={
-                <Datagrid>
-                    <TextField source="id" />
-                    <TextField source="title" />
-                    <TextField source="size" />
-                    <TextField source="url" />
-                    <ShowButton />
-                </Datagrid>
-            }
+export const FileList = props => (
+  <List
+    {...props}
+    filters={<FileFilter />}
+    bulkActions={false}
+    sort={{ field: "id", order: "DESC" }}
+  >
+    <Responsive
+      small={
+        <SimpleList
+          primaryText={record => record.id}
+          secondaryText={record => record.name}
         />
-    </List>
+      }
+      medium={
+        <Datagrid>
+          <TextField source="id" />
+          <TextField source="title" />
+          <TextField source="size" />
+          <UrlField source="url" />
+          <ShowButton />
+        </Datagrid>
+      }
+    />
+  </List>
 );
 
 const FileTitle = ({ record }) => {
-    return <span> {record ? `"${record.id}"` : ''}</span>;
+  return <span> {record ? `"${record.id}"` : ""}</span>;
 };
 
-
-export const FileCreate = (props) => (
-    <Create {...props}>
-        <SimpleForm>
-            <DisabledInput source="id" />
-            <ImageInput multiple source="pictures" accept="image/*">
-                    <ImageField source="src" title="title" />
-                </ImageInput>
-
-        </SimpleForm>
-    </Create>
+export const FileCreate = props => (
+  <Create {...props}>
+    <SimpleForm>
+      <DisabledInput source="id" />
+      <FileInput multiple source="pictures">
+        <FileField source="src" title="title" />
+      </FileInput>
+    </SimpleForm>
+  </Create>
 );
 
-export const FileShow = (props) => (
-    <Show title={<FileTitle />} {...props}>
-        <SimpleShowLayout>
-                <TextField source="id" />
-                <TextField source="title" />
-                <ImageField source="src" title="title" />
-            </SimpleShowLayout>
-    </Show>
+export const FileShow = props => (
+  <Show title={<FileTitle />} {...props}>
+    <SimpleShowLayout>
+      <TextField source="id" />
+      <TextField source="title" />
+      
+    </SimpleShowLayout>
+  </Show>
 );
