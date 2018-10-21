8cf61c945a922c45b80ae24baa2895eb24ce6854
jayTsang <beaclnd92@gmail.com>
2018-10-21 15:05:23 +0800

    实现新生成方式的密钥对和证书创建

--- src/components/keypair/index.js
+++ src/components/keypair/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {
-    FormTab, TabbedForm, ReferenceField, FormDataConsumer, RadioButtonGroupInput, SelectInput,
-    Filter, DateInput, BooleanField,
-    UrlField, DateField, FileInput, FileField, Responsive, SimpleList, List, Edit, Create, Datagrid, TextField, EditButton, DisabledInput, LongTextInput, TextInput
+    FormTab, TabbedForm, TabbedShowLayout, Tab, ReferenceField, FormDataConsumer, RadioButtonGroupInput, SelectInput,
+    Filter, DateInput, BooleanField, NumberField, 
+    UrlField, DateField, FileInput, FileField, Responsive, SimpleList, List, ShowController, ShowView, Edit, Create, 
+    Datagrid, TextField, ShowButton, EditButton, DisabledInput, LongTextInput, TextInput, NumberInput
 } from 'react-admin/lib';
+import {DateTimeInput} from 'react-admin-date-inputs'
+import {required} from 'react-admin'
+import PEMTextField from './PEMTextField'
+import uuidV1 from 'uuid/v1'
 
 const KeypairFilter = props => (
     <Filter {...props}>
-        <TextInput label="pos.search" source="sn" alwaysOn />
+        <TextInput label="pos.search" source="kp.sn" alwaysOn />
         <DateInput source="createdAt" />
     </Filter>
 );
@@ -21,18 +26,19 @@ export const KeypairList = (props) => (
         <Responsive
             small={
                 <SimpleList
-                    primaryText={record => record.sn}
-                    secondaryText={record => record.desc}
+                    primaryText={record => record.kp.sn}
+                    secondaryText={record => record.cert.sn}
                     tertiaryText={record => new Date(record.createdAt).toLocaleDateString()}
                 />
             }
             medium={
                 <Datagrid>
                     <TextField source="id" />
-                    <UrlField source="sn" title="下载密钥对" />
-                    <UrlField source="sn_cert" title="下载证书" />
+                    <UrlField label='密钥对编号' source="kp.sn" title="下载密钥对" />
+                    <UrlField label='证书编号标识' source="cert.sn" title="下载证书" />
                    <BooleanField source="status" />
-                    <DateField source="createdAt" showTime />
+                    <DateField label='创建时间' source="createdAt" showTime />
+                    <ShowButton />
                     <EditButton />
                 </Datagrid>
             }
@@ -41,24 +47,66 @@ export const KeypairList = (props) => (
 );
 
 const KeypairTitle = ({ record }) => {
-    return <span> {record ? `"${record.sn}"` : ''}</span>;
+    return <span> {record ? `"${record.kp.sn}"` : ''}</span>;
 };
+
+export const KeypairShow = (props) => (
+    <ShowController title={<KeypairTitle/>} {...props}>
+        {
+            controllerProps => 
+            <ShowView {...props} {...controllerProps}>
+                <TabbedShowLayout>
+                    <Tab label="resources.keypairs.tabs.tab1">
+                        <NumberField source="id" />
+                        <TextField label='密钥对编号' source="kp.sn" />
+                        <TextField label='生成算法名称' source="kp.alg.name" />
+                        <TextField label='生成算法参数' source="kp.alg.param" />
+                    </Tab>
+                    <Tab label="resources.keypairs.tabs.tab2">
+                        <TextField label='证书拥有者(Subject)' source="cert.distinguishName"/>
+                        <TextField label='证书发行者(Issuer)' source="cert.distinguishName"/>
+                        <TextField label='序列号(Serial Number)' source="cert.sn"/>
+                        <DateField label='开始有效期(Valid From)' source="cert.validityStart" showTime/>
+                        <DateField label='终止有效期(Valid Util)' source="cert.validityEnd" showTime/>
+                        <TextField label='签名算法(Signature Alg)' source="cert.sigAlg"/>
+                        <PEMTextField label='PEM格式证书信息' source="cert.certPEM" />
+                    </Tab>
+                    <Tab label="resources.keypairs.tabs.tab3">
+                        <PEMTextField label='PEM格式公钥' source="kp.pubKeyPEM" />
+                        {
+                            controllerProps.record && controllerProps.record.kp.alg.name === 'EC' &&
+                            <TextField label='Hex格式公钥' source="kp.pubKeyHex" />
+                        }
+                        <PEMTextField label='PEM格式私钥' source="kp.prvKeyPEM" />
+                    </Tab>
+                </TabbedShowLayout>
+            </ShowView>
+        }
+    </ShowController>
+)
+
 
 export const KeypairEdit = (props) => (
     <Edit title={<KeypairTitle />} {...props}>
         <TabbedForm>
             <FormTab label="resources.keypairs.tabs.tab1">
                 <DisabledInput source="id" />
-                <DisabledInput source="sn" />
-                <DisabledInput label='生成算法名称' source="alg.name" />
-                <DisabledInput label='生成算法参数' source="alg.param" />
+                <DisabledInput label='密钥对编号' source="kp.sn" />
+                <DisabledInput label='生成算法名称' source="kp.alg.name" />
+                <DisabledInput label='生成算法参数' source="kp.alg.param" />
             </FormTab>
             <FormTab label="resources.keypairs.tabs.tab2">
-                <LongTextInput source="pub_cert" />
+                <DisabledInput label='证书拥有者(Subject)' source="cert.distinguishName"/>
+                <DisabledInput label='证书发行者(Issuer)' source="cert.distinguishName"/>
+                <DisabledInput label='序列号(Serial Number)' source="cert.sn"/>
+                <DisabledInput label='开始有效期(Valid From)' source="cert.validityStart"/>
+                <DisabledInput label='终止有效期(Valid Util)' source="cert.validityEnd"/>
+                <DisabledInput label='签名算法(Signature Alg)' source="cert.sigAlg"/>
+                <LongTextInput disabled  label='证书PEM信息' source="cert.certPEM"/>
             </FormTab>
             <FormTab label="resources.keypairs.tabs.tab3">
-                <LongTextInput source="pub_key" />
-                <LongTextInput source="prv_key" />
+                <LongTextInput disabled label='公钥PEM信息' source="kp.pubKeyPEM" />
+                <LongTextInput disabled label='私钥PEM信息' source="kp.prvKeyPEM" />
             </FormTab>
             <FormTab label="resources.keypairs.tabs.tab4">
                 <TextInput source="pwd_old" type="password" />
@@ -73,53 +121,74 @@ const createMethodChoices=[{id: 'new', name: '新生成'}, {id: 'import', name: 
 const cryptoAlgNameChoices=[{id: 'EC', name: 'EC'},{id: 'RSA', name: 'RSA'}]
 const cryptoAlgParamECChoices=[{id: 'secp256r1', name: 'secp256r1'},{id: 'secp256k1', name: 'secp256k1'}]
 const cryptoAlgParamRSAChoices=[{id: '1024', name: '1024'},{id: '2048', name: '2048'}]
+const certSignatureAlgECChoices = [{id: 'SHA1withECDSA', name: 'SHA1withECDSA'}, {id: 'SHA256withECDSA', name: 'SHA256withECDSA'}]
+const certSignatureAlgRSAChoices = [{id: 'SHA1withRSA', name: 'SHA1withRSA'}, {id: 'SHA256withRSA', name: 'SHA256withRSA'}]
 
-export const KeypairCreate = (props) => (
+export const KeypairCreate = (props) => {
+    const keypairDefaultValue={cert: {sn : parseInt(uuidV1(), 16)}}
+    return (
     <Create {...props}>
-        <TabbedForm>
+        <TabbedForm redirect="list" defaultValue={keypairDefaultValue}>
             <FormTab label="resources.keypairs.tabs.tab1">
-                {/*
-                <DisabledInput source="id" />
-                <TextInput source="sn" />
-                <TextInput source="alg" />
-                */}
-                <RadioButtonGroupInput label='新建方式' source='createMethod' choices={createMethodChoices}/>
+                <RadioButtonGroupInput label='新建方式' source='createMethod' choices={createMethodChoices} validate={required()}/>
                 <FormDataConsumer>
                     {
                         ({formData, ...rest}) => {
                             const method = formData.createMethod
-                            if(method && method === 'new')
-                                return <RadioButtonGroupInput label='非对称密钥算法' source='alg.name' choices={cryptoAlgNameChoices}/>
-                            if(method && method === 'import')
+                            if(method === 'new')
+                                return <RadioButtonGroupInput label='非对称密钥算法' source='kp.alg.name' choices={cryptoAlgNameChoices} validate={required()} {...rest}/>
+                            if(method === 'import')
                                 return (
-                                    <FileInput source="files" label="导入密钥对" accept="application/pdf">
+                                    <FileInput source="files" label="导入密钥对" accept="application/pdf" validate={required()} {...rest}>
                                         <FileField source="fimp" title="title" />
                                     </FileInput>
                                 )
-                            return null
+                            else
+                                return null
                         }
                     }
                 </FormDataConsumer>
                 <FormDataConsumer>
                     {
                         ({formData, ...rest}) => {
-                            const alg = formData.alg
+                            const kp = formData.kp
                             const method = formData.createMethod
-                            if(alg && alg.name === 'EC' && method === 'new')
-                                return <SelectInput label='曲线名' source='alg.param' choices={cryptoAlgParamECChoices} {...rest}/>
-                            if(alg && alg.name === 'RSA' && method === 'new')
-                                return <SelectInput label='密钥长度' source='alg.param' choices={cryptoAlgParamRSAChoices} {...rest} />
-                            return null
+                            if(kp && kp.alg && kp.alg.name === 'EC' && method === 'new')
+                                return <SelectInput label='曲线名' source='kp.alg.param' choices={cryptoAlgParamECChoices} validate={required()} {...rest}/>
+                            if(kp && kp.alg && kp.alg.name === 'RSA' && method === 'new')
+                                return <SelectInput label='密钥长度' source='kp.alg.param' choices={cryptoAlgParamRSAChoices} validate={required()} {...rest} />
+                            else
+                                return null
                         }
                     }
                 </FormDataConsumer>
-            </FormTab>
-            <FormTab label="resources.keypairs.tabs.tab2">
-                <LongTextInput source="pub_cert" />
-            </FormTab>
-            <FormTab label="resources.keypairs.tabs.tab3">
-                <LongTextInput source="pub_key" />
-                <LongTextInput source="prv_key" />
+                <FormDataConsumer>
+                    {
+                        ({formData, ...rest}) => {
+                            const kp = formData.kp
+                            const method = formData.createMethod
+                            if(method === 'new' && kp && kp.alg && kp.alg.name && kp.alg.param){
+                                return (
+                                    <div style={{display: 'inline-grid'}}>
+                                        <NumberInput label='证书序列号' source='cert.sn' validate={required()}/>
+                                        {
+                                            kp.alg.name === 'EC' ?
+                                                <SelectInput label='证书签名算法' source='cert.sigAlg' choices={certSignatureAlgECChoices} validate={required()} {...rest}/>
+                                                :
+                                                <SelectInput label='证书签名算法' source='cert.sigAlg' choices={certSignatureAlgRSAChoices} validate={required()} {...rest}/>
+
+                                        }
+                                        <DateTimeInput label='证书起始有效期' source='cert.validityStart' validate={required()} options={{format: 'YYYY/MM/dd, HH:mm:ss', clearable: true}}/>
+                                        <DateTimeInput label='证书终止有效期' source='cert.validityEnd' validate={required()} options={{format: 'YYYY/MM/dd, HH:mm:ss', clearable: true}}/>
+                                        <TextInput label='证书拥有者/发行者识别名' placeholder='Distinguish Name' source='cert.distinguishName' validate={required()} />
+                                    </div>
+                                )
+                            }
+                            else
+                                return null
+                        }
+                    }
+                </FormDataConsumer>
             </FormTab>
             <FormTab label="resources.keypairs.tabs.tab4">
                 <TextInput source="pwd1" type="password" />
@@ -127,4 +196,5 @@ export const KeypairCreate = (props) => (
             </FormTab>
         </TabbedForm>
     </Create>
-);
+    )
+};
--- src/dataprovider/ra-data-indexdb.js
+++ src/dataprovider/ra-data-indexdb.js
@@ -1,5 +1,6 @@
 import {Crypto} from 'rclink'
 import {IndexDBRest} from 'rclink'
+import FlatPlainObj from 'flat-plain-object'
 import {
     GET_LIST,
     GET_ONE,
@@ -18,27 +19,25 @@ const log = (type, resource, params) => {
 
 const schema = {
     users: "++id, &userName, &email",
-    keypairs: "++id, &sn, alg, createdAt, status, ownerID",
-    certificates: "++id, &sn, ownerID, &accountAddr, &keypairSN",
+    keypairs: "++id, createdAt, status, ownerID, &kp.sn, &cert.sn",
 }
 
 // Init For test
 const initData = {
     keypairs: [
-        {sn: '1316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '2316EC91876CDEE5', alg: {name: 'RSA', param: '1024'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '3316EC91876CDEE5', alg: {name: 'RSA', param: '2048'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '4316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '5316EC91876CDEE5', alg: {name: 'EC', param: 'secp256r1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '6316EC91876CDEE5', alg: {name: 'EC', param: 'secp256r1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '7316EC91876CDEE5', alg: {name: 'EC', param: 'secp256r1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '8316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '9316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '1416EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '1516EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '1616EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '1716EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
-        {sn: '1816EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '1316EC91876CDEE5', alg: {name: 'EC', param: 'secp256r1'}}, cert: {sn: '12345678'}, createdAt: new Date(), status: false, ownerID: 1},
+        {kp: {sn: '2316EC91876CDEE5', alg: {name: 'EC', param: 'secp256r1'}}, cert: {sn: '22345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '3316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '32345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '4316EC91876CDEE5', alg: {name: 'RSA', param: '1024'}}, cert: {sn: '42345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '5316EC91876CDEE5', alg: {name: 'RSA', param: '2048'}}, cert: {sn: '52345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '6316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '62345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '7316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '72345678'}, createdAt: new Date(), status: false, ownerID: 1},
+        {kp: {sn: '8316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '82345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '9316EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '92345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '1416EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '13345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '1516EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '14345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '1616EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '15345678'}, createdAt: new Date(), status: true, ownerID: 1},
+        {kp: {sn: '1716EC91876CDEE5', alg: {name: 'EC', param: 'secp256k1'}}, cert: {sn: '16345678'}, createdAt: new Date(), status: true, ownerID: 1},
     ]
 }
 
@@ -49,9 +48,10 @@ export default (type, resource, params) => {
         case GET_LIST: 
             const {page, perPage} = params.pagination
             const s = [params.sort.field, params.sort.order]
-            const r = [(page-1)*perPage, page*perPage - 1]
+            const r = [(page-1)*perPage, page*perPage - 1]  
+            const f = FlatPlainObj(params.filter, '.') 
             const query = {
-                filter: params.filter,
+                filter: f,
                 sort: s,
                 range: r
             }
@@ -69,12 +69,25 @@ export default (type, resource, params) => {
             let d = params.data
             d.status = false
             d.createdAt = new Date()
-            const keypair = Crypto.CreateKeypair(d.alg.name, d.alg.param)
+
+            const keypair = Crypto.CreateKeypair(d.kp.alg.name, d.kp.alg.param)
             const prvKeyPEM = Crypto.GetKeyPEM(keypair.prvKeyObj)
             const pubKeyPEM = Crypto.GetKeyPEM(keypair.pubKeyObj)
-            d.prvKeyPEM = prvKeyPEM
-            d.pubKeyPEM = pubKeyPEM
-            d.sn = Crypto.GetHashVal(Crypto.GetHashVal(prvKeyPEM), 'RIPEMD160').toString('base64')
+            d.kp.prvKeyHex = keypair.prvKeyObj.prvKeyHex
+            d.kp.prvKeyPEM = prvKeyPEM
+            d.kp.pubKeyHex = keypair.pubKeyObj.pubKeyHex
+            d.kp.pubKeyPEM = pubKeyPEM
+            d.kp.sn = Crypto.GetHashVal(Crypto.GetHashVal(pubKeyPEM), 'RIPEMD160').toString('hex')
+
+            let startUnixTimeStr = d.cert.validityStart.getTime().toString()
+            startUnixTimeStr = startUnixTimeStr.slice(0, -3)
+            let endUnixTimeStr = d.cert.validityEnd.getTime().toString()
+            endUnixTimeStr = endUnixTimeStr.slice(0, -3)
+
+            const certPEM = Crypto.CreateSelfSignedCertificate(d.cert.sn, d.cert.sigAlg, d.cert.distinguishName, 
+                startUnixTimeStr, endUnixTimeStr, keypair)
+            d.cert.certPEM = certPEM
+
             return indexdbRest.create(resource, d).then(r => ({data: r.result}))
         case UPDATE:
             const uID = parseInt(params.id, 10)
