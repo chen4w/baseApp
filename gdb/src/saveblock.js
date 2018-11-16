f33b6f1e6398341beda708328d843acb0ff7380c
jiangbuyun@sina.com <jiangbuyun@sina.com>
2018-11-16 10:16:25 +0800

    modify sync save class,add table fields

--- gdb/src/saveblock.js
+++ gdb/src/saveblock.js
@@ -1,117 +1,196 @@
 const ECODE_BIN = 'binary';
 const ECODE_HEX = 'hex';
-const NET_DEFAULT = "DEFAULT_NET";
-const { RestAPI } = require('./rclink/rest')
+const { RestAPI } = require('./rclink/rest');
+const {URL} = require('url');
 
-function updateNetInfo(prisma, info, netId) {
-  prisma.mutation.updateNetwork({
-    where: { id: netId },
-    data: info
-  });
-}
+class BlockStorager{
 
-var default_NetId;
-var blockCount = 0;
-var transCount = 0;
+  constructor(prisma,pullUrl) {
+    this.prisma = prisma;
+    this.default_NetId =  "";
+    this.default_NetName = "DEFAULT_NET";
+    var p = URL.parse(pullUrl);
+    this.seedip = p.hostname;
+    console.log(this.seedip);
+    
+    this.lastBlockHash = '';
+    this.lastheight = 0;
+    this.transCount = 0;
+    this.lastBlockTime = 0;
+    this.tps = 0.0;
+  }
 
-async function getNetInfo(api_url,prisma, netName) {
-  const ra = new RestAPI(api_url);
-  await ra.chaininfo().then(ci=>{
-    blockCount = parseInt(ci.result.height);
-    transCount = parseInt(ci.result.totalTransactions);
-  });
-  //检索组网记录，如果不存在创建之
-  const name = netName || NET_DEFAULT;
-  var nets, net0;
-  await prisma.query.networks({ where: { name: name } }).then(result => {
-    nets = result
-  }).catch(err => console.log(err));
-
-  if (nets.length > 0) {
-    default_NetId = nets[0].id;
-    return nets[0];
-  } else {
-    await prisma.mutation.createNetwork({
-      data: {
-        name: name,
-        syncHeight: 1,
-        seedip: "",
-        blockCount: 0,
-        transCount: 0
+  async InitStorager() {
+    var net = null;
+    await this.prisma.query.networks({ where: { seedip: this.seedip } }).then(result => {
+      if(result != null && result.length > 0){
+        net = result[0];
       }
-    }).then(result => { net0 = result }).catch(err => console.log(err));
-    default_NetId = net0.id;
-    return net0;
+    }).catch(err => console.log(err));
+
+    if(net == null){
+      await this.prisma.mutation.createNetwork({
+        data: {
+          name: this.default_NetName,
+          syncHeight: 0,
+          seedip: this.seedip,
+          lastBlockHash:'',
+          lastBlockTime:0,
+          tps:0.0,
+          blockCount: 0,
+          transCount: 0
+        }
+      }).then(result => { net = result }).catch(err => console.log(err));
+    }
+
+    if(net != null){
+      this.default_NetId = net.id;
+      this.default_NetName = net.name;
+      this.lastheight = net.syncHeight;
+      this.lastBlockHash = net.lastBlockHash;
+      this.transCount = net.transCount;
+      this.lastBlockTime = net.lastBlockTime;
+      this.tps = net.tps;
+    }
+    console.log('storager init finish');
+  }
+
+  getLastHeight(){
+    retrun this.lastheight;
+  }
+
+  getLastBlockHash(){
+    return this.lastBlockHash;
+  }
+
+
+  async saveBlock(blk,blkHash){
+    var blk_data = {
+      hash: blkHash,
+      transCount: blk.transactions.length,
+      timeStamp: new Date(blk.timestamp.seconds * 1000 - 8 * 3600 * 1000),
+      preHash: null
+    };
+    if (blk.previousBlockHash)
+      blk_data.preHash = Buffer.from(blk.previousBlockHash).toString(ECODE_BIN);
+    if(blk_data.preHash == this.lastBlockHash){
+      //可以保存,继续检查该块是否存在
+      this.prisma.exists.Block({ hash: blk_data.hash }).then(b_exist => {
+        if (!b_exist) {
+            //不存在可以继续保存，先保存交易，后保存块
+            saveTranscations(txs,0).then(rdata => {
+              if(rdata.result){
+                await prisma.mutation.createBlock(
+                  {
+                    data: blk_data,
+                  }
+                );
+                refreshCache(blk,blkHash);
+                console.log('save block success,block height='+this.lastheight);
+                return true;
+              }else{
+                console.log('save transcation failed');
+                return false;
+              }
+            });
+        } else {
+          //已经存在了，修改最后缓存信息
+          refreshCache(blk,blkHash);
+          console.log('block[' + prevHash + '] exists.');
+          return true;
+        }
+      });
+    }else{
+      console.log('do not save ,current block is not next block,reset pull,please');
+      return false;
+    }
+  }
+
+  async refreshCache(blk,blkHash){
+    var net_data = statisNetData(blk,blkHash);
+    await this.prisma.mutation.updateNetwork({
+      where: { id: netId },
+      data: info
+    });
+    this.lastheight = net_data.syncHeight;
+    this.lastBlockHash = net_data.lastBlockHash;
+    this.transCount = net_data.transCount;
+    this.lastBlockTime = net_data.lastBlockTime;
+    this.tps = net_data.tps;
+  }
+
+  statisNetData(blk,blkHash){
+    var tps =0.0;
+    var ctime = blk.timestamp.toInt();
+    var tm_span = ctime - this.lastBlockTime;
+    //if(tm_span < 10000){
+      tps = blk.transactions.length / tm_span;
+      tps = Math.floor(tps * 10) / 10;
+    /*}else{
+      tps = this.tps;
+    }*/
+
+    var net_data = { 
+      syncHeight: this.syncHeight+1, 
+      transCount: this.transCount + blk.transactions.length, 
+      lastBlockHash:blkHash,
+      lastBlockTime:ctime,
+      tps: tps
+    }
+    return net_data;
+  }
+  
+  saveTranscations(txs,cidx){
+    return new Promise(function (resolve, reject) {
+      if(txs != null && txs.length > cidx){
+        var tx = txs[cidx];
+        var tx_data = {
+          txId: tx.txid,
+          blockId: blk_data.hash,
+          type: tx.type,
+          cname: tx.payload.chaincodeID.name,
+          action: tx.payload.ctorMsg.function,
+          ipt: tx.payload.ctorMsg.args.toString(),
+          signature: tx.signature.toString(ECODE_HEX),
+          //nodejs只支持到毫秒
+          timeStamp: new Date(tx.timestamp.seconds * 1000 - 8 * 3600 * 1000),
+          blocker: { connect: { id: obj_blk.id } }
+        };
+
+        this.prisma.exists.Transaction({ txId: tx.txid }).then(t_exist => {
+          if (!t_exist) {
+            this.prisma.mutation.createTransaction(
+              {
+                data: tx_data,
+              }
+            ).then((data)=>{
+              if((txs.length - 1) == cidx){
+                resolve({result: true});
+              }else{
+                saveTranscations(txs,cidx+1);
+              }
+            }).catch(err => {
+                console.log(err);
+                resolve({result: false});
+            });
+          } else {
+              console.log('transcation[' + tx.txid + '] exists.');
+              if((txs.length - 1) == cidx){
+                resolve({result: true});
+              }else{
+                this.saveTranscations(txs,cidx+1);
+              }
+          }
+        }).catch(err =>{
+          console.log(err);
+          resolve({result: false});
+        });
+      }else{
+        resolve({result: true});
+      }
+    }
   }
 }
 
-var tm_lastBlock = null;
-async function saveBlock(blk, prisma, netId) {
-  var blk_data = {
-    hash: Buffer.from(blk.stateHash).toString(ECODE_BIN),
-    transCount: blk.transactions.length,
-    timeStamp: new Date(blk.timestamp.seconds * 1000 - 8 * 3600 * 1000),
-    preHash: null
-  };
-  if (blk.previousBlockHash)
-    blk_data.preHash = Buffer.from(blk.previousBlockHash).toString(ECODE_BIN);
+exports.BlockStorager = BlockStorager;
 
-  //写入区块,
-  var obj_blk = await prisma.mutation.createBlock(
-    {
-      data: blk_data,
-    }
-  )
-  var txs = blk.transactions;
-  //写入包含的交易
-  for (var i = 0; i < txs.length; i++) {
-    var tx = txs[i];
-    var tx_data = {
-      txId: tx.txid,
-      blockId: blk_data.hash,
-      type: tx.type,
-      cname: tx.payload.chaincodeID.name,
-      action: tx.payload.ctorMsg.function,
-      ipt: tx.payload.ctorMsg.args.toString(),
-      signature: tx.signature.toString(ECODE_HEX),
-      //nodejs只支持到毫秒
-      timeStamp: new Date(tx.timestamp.seconds * 1000 - 8 * 3600 * 1000),
-      blocker: { connect: { id: obj_blk.id } }
-    }
-    prisma.mutation.createTransaction(
-      {
-        data: tx_data,
-      }
-    )
-    //console.log(tx_data)
-  }
-  //来自push,新出区块
-  if(!netId){
-    blockCount += 1;
-    transCount += blk.transactions.length;
-    var tm_now = process.uptime();
-    var tps =0;
-    if(tm_lastBlock!==null){
-      var tm_span = tm_now - tm_lastBlock;
-      console.log('tm_span:'+ tm_span);
-
-      if(tm_span < 10000){
-        tps = blk.transactions.length / tm_span;
-        tps = Math.floor(tps * 10) / 10 
-      }
-    }
-    console.log('tps:'+ tps);
-    tm_lastBlock = tm_now;
-    console.log('tm_lastBlock:'+ tm_lastBlock);
-
-    updateNetInfo(
-      prisma,
-      { blockCount: blockCount, transCount: transCount, tps: tps},
-      default_NetId)  
-  }
-  //console.log(blk_data)  
-}
-
-module.exports.saveBlock = saveBlock;
-module.exports.updateNetInfo = updateNetInfo;
-module.exports.getNetInfo = getNetInfo;
--- src/server/deployservice/test/testRW4Hocon.js
+++ src/server/deployservice/test/testRW4Hocon.js
@@ -1,4 +1,4 @@
-var parseHocon = require('hocon-parser');
+/*var parseHocon = require('hocon-parser');
 var fs =require("fs");
 var filename = 'C:\\RepChain_devlop_env\\scala_workspace\\repchain\\conf\\system.conf';
 var wrfilename = 'e:\\tempfile.txt';
@@ -14,7 +14,19 @@ fs.readFile(filename,{flag:'r+',encoding:'utf-8'},function(err,data){
            console.log("文件写入成功");
         })
     }
-})
+})*/
+
+var URL = require('url');
+var urlstr = 'http://localhost:4466';
+var p = URL.parse(urlstr);
+console.log(p.hostname);
 
 
+var crypto = require('crypto');
+var sha = crypto.createHash('SHA256');
+
+var message = 'hello';
+var digest = sha.update(message, 'utf8').digest('hex'); 
+
+console.log(digest);
 
